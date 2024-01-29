5fe71be15072616f71fa60f84530d23ef731de9e
ArchAngel776 <karol.marciniak155@gmail.com>
2024-01-29 17:02:18 +0100

    Make Text component selectable from builder position.

--- lib/gui/element/text.py
+++ lib/gui/element/text.py
@@ -1,8 +1,19 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPalette
 from PyQt5.QtWidgets import QLabel
 
+from lib.decorators import method
+from lib.decorators.decorator import Decorator
 from lib.hooks import palette_color
 from lib.gui.element import Element
+
+
+# Decorators
+
+class SelectableCursor(Decorator):
+    def method(self, target, selectable=True):
+        super().method(target, selectable)
+        return target.Cursor(Qt.IBeamCursor) if selectable else target.Cursor(Qt.ArrowCursor)
 
 
 # Main
@@ -11,6 +22,14 @@ class Text(QLabel, Element):
     def __init__(self, root, text=None):
         super().__init__(root)
         self.setText(text)
+
+    @method(SelectableCursor)
+    def Selectable(self, selectable=True):
+        if selectable:
+            self.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+        else:
+            self.setTextInteractionFlags(Qt.TextInteractionFlag.NoTextInteraction)
+        return self
 
     def Color(self, color):
         self.setPalette(palette_color(QPalette.WindowText, color))
--- lib/gui/element/text.pyi
+++ lib/gui/element/text.pyi
@@ -1,17 +1,32 @@
-from typing import Self
+from typing import Self, TypeVar
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QLabel
 
+from lib.decorators import method
+from lib.decorators.decorator import Decorator
 from lib.gui.element import Element
 from lib.gui.window import Window
+
+# Types
+
+TTextSelectableCursor = TypeVar("TTextSelectableCursor", bound=Text)
+
+
+# Decorators
+
+class SelectableCursor(Decorator[Text, [Text, bool]]):
+    def method(self, target: TTextSelectableCursor, selectable: bool = True) -> TTextSelectableCursor: ...
 
 
 # Main
 
 class Text(QLabel, Element):
     def __init__(self, root: Window, text: str = None) -> None: ...
+
+    @method(SelectableCursor)
+    def Selectable(self, selectable: bool = True) -> Self: ...
 
     def Color(self, color: QColor) -> Self: ...
 
